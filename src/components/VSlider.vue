d76f82b39386e6c112915dff5d0c69669b671797
Kristjan Jansen <kristjan.jansen@gmail.com>
2021-12-08 11:46:29 +0200

    feat: cleanup main example (#2)

--- src/components/VSlider.vue
+++ src/components/VSlider.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { defineProps, computed } from "vue";
+import { computed } from "vue";
 const props = defineProps(["modelValue"]);
 const emit = defineEmits(["update:modelValue"]);
 const progress = computed({
