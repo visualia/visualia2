330d02ad4a5474c4886fded6bdad2894fc90e293
kristjanjansen <kristjan.jansen@gmail.com>
2021-12-08 11:16:59 +0200

    feat: vite starter

---
+++ examples/vite/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue'
+import { VSlider, hsla } from "visualia2";
+
+const a = ref(0);
+</script>
+
+<template>
+  <VSlider v-model="a" />
+  <br />
+  <input type="range" v-model="a" /> 
+  <br />
+  {{ hsla(a) }}
+</template>
+
