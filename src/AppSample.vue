aec421f4ebdb4544cd699d7fe8996c9d2099d50d
kristjanjansen <kristjan.jansen@gmail.com>
2021-12-08 11:33:43 +0200

    chore: reformat main example

--- src/AppSample.vue
+++ src/AppSample.vue
@@ -1,21 +1,17 @@
 <script setup>
-import { hsla, VSlider } from "./visualia.js";
-let radius = $ref(50);
-let multipliers = [1, 1.5, 2];
-let color = $computed(() => hsla(radius));
+import { VSlider, hsla } from "./visualia.js";
+
+let a = $ref(0);
 </script>
 
 <template>
-  <VSlider v-model="radius" />
-  <input type="range" v-model="radius" />
+  <VSlider v-model="a" />
+  <br />
+  <input type="range" v-model="a" />
+  <br />
+  {{ hsla(a) }}
+  <br />
   <svg width="200" height="200">
-    <circle
-      v-for="multiplier in multipliers"
-      cx="100"
-      cy="100"
-      :r="radius * multiplier"
-      opacity="0.5"
-      :fill="color"
-    />
+    <circle cx="100" cy="100" r="50" :fill="hsla(a)" />
   </svg>
 </template>
