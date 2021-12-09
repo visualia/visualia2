92f72056a62e7e8274d336f2f4b32bc321a754a9
kristjanjansen <kristjan.jansen@gmail.com>
2021-12-09 10:46:44 +0200

    math

--- src/AppSample.vue
+++ src/AppSample.vue
@@ -1,10 +1,12 @@
 <script setup>
-import { VSlider, hsla } from "./visualia.js";
+import { VSlider, VMath, hsla } from "./visualia.js";
 
 let a = $ref(0);
 </script>
 
 <template>
+  <VMath>a = \sqrt{ {{ a }} }</VMath>
+  <br />
   <VSlider v-model="a" />
   <br />
   <input type="range" v-model="a" />
@@ -15,3 +17,7 @@ let a = $ref(0);
     <circle cx="100" cy="100" r="50" :fill="hsla(a)" />
   </svg>
 </template>
+
+<style>
+@import url("https://unpkg.com/katex/dist/katex.css");
+</style>
--- src/visualia.js
+++ src/visualia.js
@@ -1,3 +1,4 @@
+export { default as VMath } from "./components/VMath.vue";
 export { default as VSlider } from "./components/VSlider.vue";
 
 export * from "./utils/index.js";
