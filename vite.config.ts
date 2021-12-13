18f9f74b616f0ead960d133dd36f6e3edd2f1dd9
Kristjan Jansen <kristjan.jansen@gmail.com>
2021-12-13 13:40:15 +0200

    feat: update deps (#6)

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,10 @@ import { defineConfig } from "vite";
 import path from "path";
 import vue from "@vitejs/plugin-vue";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue({
-      script: {
-        refTransform: true,
-        propsDestructureTransform: true,
-      },
+      refTransform: true
     }),
   ],
   build: {
