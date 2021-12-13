cb71015cb2547d3415c4ed2ebf7172876878fcd7
kristjanjansen <kristjan.jansen@gmail.com>
2021-12-13 13:39:49 +0200

    fix: reftransform

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,7 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   plugins: [
     vue({
-      script: {
-        reactivityTransform: true
-      },
+      refTransform: true
     }),
   ],
   build: {
