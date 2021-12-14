17e2642acfef512c65050af640f85b4cdd4ca953
kristjanjansen <kristjan.jansen@gmail.com>
2021-12-14 11:42:18 +0200

    chore: hm

--- src/visualia.js
+++ src/visualia.js
@@ -1,4 +1,20 @@
+import { defineComponent } from "vue";
 export { default as VSlider } from "./components/VSlider.vue";
 export { default as VMath } from "./components/VMath.vue";
+import VSlider from "./components/VSlider.vue";
+import VMath from "./components/VSlider.vue";
+export const components = { VSlider, VMath };
 
+import * as utils from "./utils/index.js";
 export * from "./utils/index.js";
+
+export const Visualia = {
+  install: (app) => {
+    Object.entries(components).forEach(([name, component]) => {
+      app.component(name, defineComponent(component));
+    });
+    Object.entries(utils).forEach(
+      ([name, util]) => (app.config.globalProperties[name] = util)
+    );
+  },
+};
