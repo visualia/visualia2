539a39bfc534f72beb1a331d78dd95596d03a1b0
kristjanjansen <kristjan.jansen@gmail.com>
2021-12-21 10:55:15 +0200

    fix: rm VCompiler for now

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
-export { default as VCompiler } from "./VCompiler.vue";
 export { default as VMath } from "./VMath.vue";
 export { default as VSlider } from "./VSlider.vue";
 export { default as VSvg } from "./VSvg.vue";
